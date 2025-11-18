5094fc0ac38321c73e880393beadec95156507a0
hilal555 <muhamadhilal04@gmail.com>
2025-11-19 00:10:07 +0700

    hapus button filter

--- resources/js/Pages/SuperAdmin/KelolaSurat/components/FiltersBar.tsx
+++ resources/js/Pages/SuperAdmin/KelolaSurat/components/FiltersBar.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/Components/ui/button';
 import { Input } from '@/Components/ui/input';
 import {
     Select,
@@ -32,37 +31,29 @@ export default function FiltersBar({
 
     return (
         <div className="flex flex-col gap-4 md:flex-row md:items-center">
-        <div className="flex-1">
-            <div className="relative">
-                <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-slate-400" />
-                <Input
-                    placeholder="Cari surat..."
+            <div className="flex-1">
+                <div className="relative">
+                    <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-slate-400" />
+                    <Input
+                        placeholder="Cari surat..."
                         className="pl-9"
                         value={search}
                         onChange={(event) => onSearchChange(event.target.value)}
                     />
+                </div>
             </div>
-        </div>
-        <Select value={category} onValueChange={onCategoryChange}>
-            <SelectTrigger className="w-full md:w-48">
-                <SelectValue placeholder="Semua" />
-            </SelectTrigger>
-            <SelectContent>
-                {categoryOptions.map((option) => (
-                    <SelectItem key={option} value={option}>
-                        {option === 'all' ? 'Semua' : option}
+            <Select value={category} onValueChange={onCategoryChange}>
+                <SelectTrigger className="w-full md:w-48">
+                    <SelectValue placeholder="Semua" />
+                </SelectTrigger>
+                <SelectContent>
+                    {categoryOptions.map((option) => (
+                        <SelectItem key={option} value={option}>
+                            {option === 'all' ? 'Semua' : option}
                         </SelectItem>
                     ))}
                 </SelectContent>
             </Select>
-            <Button
-                type="button"
-                variant="outline"
-                className="w-full md:w-auto"
-            >
-                <Filter className="mr-2 h-4 w-4" />
-                Filter
-            </Button>
         </div>
     );
 }
